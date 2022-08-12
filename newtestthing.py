776ec5be3ca7f1f4bf14702ae5fe714461778fef
Cody Gula <cgula7@gmail.com>
2022-08-11 19:26:44 -0700

    still not working

--- newtestthing.py
+++ newtestthing.py
@@ -12,35 +12,76 @@ class Asteroid():
     def __init__(self, asteroidX, asteroidY):
         self.asteroidX = asteroidX
         self.asteroidY = asteroidY
-        self.asteroidChangeX = 1
-        self.asteroidChangeY = 1
-
+        
+    def display(self):    
+        screen.blit((Asteroid.asteroidImg), (self.asteroidX, self.asteroidY))
     
-    def create(asteroidX, asteroidY):
-        asteroidX = asteroidX
-        asteroidY = asteroidY
-        screen.blit(pygame.image.load("asteroid.png"), (asteroidX, asteroidY))
+    def move(self):
+        pass
+    
+    
+
+ 
 
 
 
 
 running = True
+ax = 100
+
+startX = random.randint(5,790)
+startY = random.randint(5, 590)
+
+def create():
+        ax = random.randint(5,790)
+        ay = random.randint(5, 590)
+        asteroids.append(Asteroid(ax,ay))
+
+list1 = ["item1", "item2", "item3", "item4"]
+asteroids = []
+for i in list1:
+    i = create()
+
+
+    
+
 
 while running:
 
     # Background color
     screen.fill((255,0,0))
+    
 
+    #xchange = 1
+    for i in asteroids:
+        xchange = 1
+        if i.asteroidX < 150:
+            xchange = 1
+            i.asteroidX += xchange
+            i.display()
+            print(i.asteroidX)
+        elif i.asteroidX > 150:
+            xchange = -1
+            i.asteroidX += xchange
+            i.display()
+       
 
-    asteroid = Asteroid.create(100,100)
-    asteroid.asteroidX += asteroid.asteroidChangeX
+    # enemyX += enemyX_change
 
     # if enemyX <= 0:
     #     enemyX_change = 0.3
-    #     enemyY += enemyY_change
+        
     # elif enemyX >= 736:
     #     enemyX_change = -0.5
-    #     enemyY += enemyY_change
+        
+        Asteroid.display(i)
+
+
+    #     print("moving", asteroid.asteroidX, ax)
+        #ax = asteroid.asteroidX + asteroid.asteroidX
+        
+    
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
